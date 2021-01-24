bb3106975392dd23005aea8e02c1d7dbd0424963
Laks <lakhsmycastro336@gmail.com>
2021-01-23 23:53:47 -0300

    🌱 Chore(Lefthook): Export bin script instead use internal script

--- .lefthook/prepare-commit-msg/template_checker.dart
+++ .lefthook/prepare-commit-msg/template_checker.dart
@@ -1,6 +1,1 @@
-import '../../bin/git/hooks/prepare_commit_msg/template_checker.dart'
-    as template_checker;
-
-void main(List<String> args) {
-  template_checker.main(args);
-}
+export '../../bin/git/hooks/prepare_commit_msg/template_checker.dart';
