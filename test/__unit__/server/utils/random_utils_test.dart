0720ce7ea9f3033ab93dd35185079f99820813e6
Laks <lakhsmycastro336@gmail.com>
2021-01-24 18:52:05 -0300

    ⚠️ Test: Improve tests for random and string utils

--- test/__unit__/server/utils/random_utils_test.dart
+++ test/__unit__/server/utils/random_utils_test.dart
@@ -3,39 +3,51 @@ import 'package:anime_dart/server/utils/random_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final precision = 5000;
+
   group('randomInt() method', () {
     test('positive numbers', () {
       final min = 0, max = 5;
 
       final results = <int>[
-        for (var i = 0; i < 500; i++) randomInt(min: min, max: max)
+        for (var i = 0; i < precision; i++) randomInt(min: min, max: max)
       ];
 
-      expect(results.every((result) => result >= min && result <= max), true);
+      expect(results.every((result) => result >= min && result < max), true);
     });
     test('negative numbers', () {
       final min = -10, max = 0;
 
       final results = <int>[
-        for (var i = 0; i < 500; i++) randomInt(min: min, max: max)
+        for (var i = 0; i < precision; i++) randomInt(min: min, max: max)
       ];
 
-      expect(results.every((result) => result >= min && result <= max), true);
+      expect(results.every((result) => result >= min && result < max), true);
     });
     test('zero range', () {
       final value = -10;
 
       final results = <int>[
-        for (var i = 0; i < 500; i++) randomInt(min: value, max: value),
+        for (var i = 0; i < precision; i++) randomInt(min: value, max: value),
       ];
 
       expect(results.every((result) => result == value), true);
+    });
+    test('max and min values', () {
+      final min = 0, max = 2;
+
+      final results = <int>[
+        for (var i = 0; i < precision; i++) randomInt(min: min, max: max),
+      ];
+
+      expect(results.every((result) => result < max), true);
+      expect(results.any((result) => result == min), true);
     });
     test('invalid range', () {
       final min = -10, max = -20;
 
       List<int> getResults() => <int>[
-            for (var i = 0; i < 500; i++) randomInt(min: min, max: max),
+            for (var i = 0; i < precision; i++) randomInt(min: min, max: max),
           ];
 
       expect(getResults, throwsA(isA<AssertionError>()));
@@ -44,7 +56,7 @@ void main() {
   group('randomDouble() method', () {
     test('positive numbers', () {
       final results = <double>[
-        for (var i = 0; i < 500; i++) randomDouble(min: 0.0, max: 5.0)
+        for (var i = 0; i < precision; i++) randomDouble(min: 0.0, max: 5.0)
       ];
 
       expect(results.every((result) => result >= 0.0 && result <= 5.0), true);
@@ -55,17 +67,17 @@ void main() {
       final min = -10.0, max = 0.0;
 
       final results = <double>[
-        for (var i = 0; i < 500; i++) randomDouble(min: min, max: max)
+        for (var i = 0; i < precision; i++) randomDouble(min: min, max: max)
       ];
 
-      expect(results.every((result) => result >= min && result <= max), true);
+      expect(results.every((result) => result >= min && result < max), true);
       expect(results.any((result) => result % 1 != 0.0), true);
     });
     test('zero range', () {
       final value = -10.5;
 
       final results = <double>[
-        for (var i = 0; i < 500; i++) randomDouble(min: value, max: value)
+        for (var i = 0; i < precision; i++) randomDouble(min: value, max: value)
       ];
 
       expect(results.every((result) => result == value), true);
@@ -74,10 +86,20 @@ void main() {
       final min = -10.0, max = -15.0;
 
       List<double> getResults() => <double>[
-            for (var i = 0; i < 500; i++) randomDouble(min: min, max: max),
+            for (var i = 0; i < precision; i++)
+              randomDouble(min: min, max: max),
           ];
 
       expect(getResults, throwsA(isA<AssertionError>()));
+    });
+    test('max and min values', () {
+      final min = 0.0, max = 2.0;
+
+      final results = <double>[
+        for (var i = 0; i < precision; i++) randomDouble(min: min, max: max),
+      ];
+
+      expect(results.every((result) => result < max), true);
     });
   });
 }
